1787240d3b84633f928f9a46cfdd3aba179bd392
Nam Hoang <hoangnam091196@gmail.com>
2024-10-19 23:07:13 +0700

    add UserProfilePage, edit info user

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,19 +2,30 @@ import HomePage from "@/pages/HomePage";
 import Layout from "@/layouts/Layout";
 import AuthCallbackPage from "@/pages/AuthCallbackPage";
 import { Navigate, Route, Routes } from "react-router-dom";
+import UserProfilePage from "@/pages/UserProfilePage";
+import ProtectedRoute from "@/auth/ProtectedRoute";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<>user profile</>} />
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,11 +1,41 @@
+import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type CreateUserRequest = {
   auth0Id: string;
   email: string;
+};
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const getMyUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Get current user fail");
+    }
+    return response.json();
+  };
+
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+  return { currentUser, isLoading };
 };
 
 export const useCreateMyUser = () => {
@@ -31,4 +61,46 @@ export const useCreateMyUser = () => {
     isSuccess,
   } = useMutation(createMyUserRequest);
   return { createUser, isLoading, isError, isSuccess };
+};
+
+type UpdateDataMyUser = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+
+export const useUpDataMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const updateMyUserRequest = async (formData: UpdateDataMyUser) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+  };
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User profile updated!");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+  return { updateUser, isLoading };
 };
